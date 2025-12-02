bd832512b9d738219b1b329361024e8f2cde2783
Arturo Hernandez <Arturo.Hernandez.B@ibm.com>
2025-12-02 15:34:23 -0600

    latest

---
+++ pub/service.js
@@ -0,0 +1,17 @@
+app.service('myserv', function() {
+          this.getServiceList = function () {
+    return [];
+}
+this.getISEndpoint = function() { 
+ return 'http://arturos-air.hdez.cloud:5555/';
+}
+this.getAPIList = function() { 
+ return [];
+}
+this.getCreatedTime = function() { 
+ return "02-12-2025 15:29:59 CST";
+}
+this.getPackageInfo = function(){
+ return{"packageName":"SAPDriversProject","createdDate":"01-12-2025 12:51:46 CST","version":"1.0"};
+}
+});
